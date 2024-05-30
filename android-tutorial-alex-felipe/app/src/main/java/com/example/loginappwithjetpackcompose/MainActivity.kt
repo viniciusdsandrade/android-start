1dbc20a4d72181b9dd6bf1534c4f9b04ca73cb8d
Andrade <vinicius_andrade2010@hotmail.com>
2024-05-29 22:44:36 -0300

    feat: tela de login

--- android-tutorial-alex-felipe/app/src/main/java/com/example/loginappwithjetpackcompose/MainActivity.kt
+++ android-tutorial-alex-felipe/app/src/main/java/com/example/loginappwithjetpackcompose/MainActivity.kt
@@ -4,44 +4,127 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.loginappwithjetpackcompose.ui.theme.LoginAppWithJetpackComposoTheme
+import androidx.compose.ui.unit.dp
+import com.example.loginappwithjetpackcompose.ui.theme.LoginAppWithJetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            LoginAppWithJetpackComposoTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            LoginAppWithJetpackComposeTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    AuthScreen()
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun AuthScreen() {
+    val textFieldWidth = 280.dp // Defina a largura desejada aqui
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        var username by remember { mutableStateOf("") }
+        var password by remember { mutableStateOf("") }
+        var passwordVisible by remember { mutableStateOf(false) }
+
+        // Campo de Usuário com ícone
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(0.98f)
+                .padding(4.dp)
+        ) {
+            TextField(
+                value = username,
+                onValueChange = { newValue -> username = newValue },
+                label = { Text("Usuário") },
+                leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Default.Person,
+                        contentDescription = "Ícone de Usuário"
+                    )
+                },
+                modifier = Modifier
+                    .width(textFieldWidth) // Largura fixa
+                    .height(56.dp) // Altura fixa
+                    .weight(1f) // Ocupa o espaço disponível no Row
+            )
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        // Campo de Senha com ícone e botão de visibilidade
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(0.98f)
+                .padding(4.dp)
+        ) {
+            TextField(
+                value = password,
+                onValueChange = { password = it },
+                label = { Text("Senha") },
+                leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Default.Lock,
+                        contentDescription = "Ícone de Senha"
+                    )
+                },
+                trailingIcon = {
+                    IconButton(onClick = { passwordVisible = !passwordVisible }) {
+                        Icon(
+                            imageVector = if (passwordVisible) Icons.Filled.Clear else Icons.Filled.Refresh,
+                            contentDescription = if (passwordVisible) "Hide password" else "Show password"
+                        )
+                    }
+                },
+                visualTransformation = if (passwordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+                modifier = Modifier
+                    .width(textFieldWidth) // Largura fixa
+                    .height(56.dp) // Altura fixa
+                    .weight(1f) // Ocupa o espaço disponível no Row
+            )
+        }
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        Button(onClick = { /* TODO: Implementar lógica de login */ }) {
+            Text(text = "Entrar")
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun GreetingPreview() {
-    LoginAppWithJetpackComposoTheme {
-        Greeting("Android")
+fun AuthScreenPreview() {
+    LoginAppWithJetpackComposeTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            AuthScreen()
+        }
     }
 }
