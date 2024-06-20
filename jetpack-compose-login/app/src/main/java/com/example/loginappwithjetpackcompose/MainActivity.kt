18be189aae374a12fece9937790e4fbe84d9f7b3
Vinicius <vinicius_andrade2010@hotmail.com>
2024-06-20 16:14:41 -0300

    feat: tela de login com logo e login alternativo

--- jetpack-compose-login/app/src/main/java/com/example/loginappwithjetpackcompose/MainActivity.kt
+++ jetpack-compose-login/app/src/main/java/com/example/loginappwithjetpackcompose/MainActivity.kt
@@ -2,13 +2,17 @@ package com.example.loginappwithjetpackcompose
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.absolutePadding
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -16,31 +20,39 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Block
+import androidx.compose.material.icons.filled.CheckCircle
+import androidx.compose.material.icons.filled.CheckCircleOutline
 import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.RemoveRedEye
 import androidx.compose.material3.Button
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.loginappwithjetpackcompose.TipoAutenticacao.EMAIL
 import com.example.loginappwithjetpackcompose.ui.theme.LoginAppWithJetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -53,19 +65,24 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    AuthScreen(onEnterClick = {
-                        Log.i("MainActivity", "User: $it")
-                    })
+                    AuthScreen(onLoginClick = {
+                        Log.i("MainActivity", "SignIn")
+                    },
+                        onSignUpClick = {
+                            Log.i("MainActivity", "SignUp")
+                        }
+                    )
                 }
             }
         }
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
+
 @Composable
 fun AuthScreen(
-    onEnterClick: (User) -> Unit
+    onLoginClick: () -> Unit,
+    onSignUpClick: () -> Unit
 ) {
     val textFieldWidth = 280.dp // Defina a largura desejada aqui
     val usernameIconSize = 23.dp
@@ -78,11 +95,30 @@ fun AuthScreen(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        var username by remember { mutableStateOf("") }
-        var password by remember { mutableStateOf("") }
-        var passwordVisible by remember { mutableStateOf(false) }
 
-        // Campo de Usuário com ícone
+        val (username, setUsername) = rememberSaveable {
+            mutableStateOf("")
+        }
+        val (password, setPassword) = rememberSaveable {
+            mutableStateOf("")
+        }
+        var passwordVisible by remember { mutableStateOf(false) }
+        var checkRememberButton by remember { mutableStateOf(false) }
+        val context = LocalContext.current
+        // val isFieldsEmpty = username.isNotEmpty() && password.isNotEmpty()
+
+        Image(
+            painter = painterResource(id = R.drawable.logo_discerno_pet_escuro),
+            contentDescription = "Logo Discerno Pet",
+            modifier = Modifier
+                .absolutePadding(top = 60.dp)
+                .size(180.dp)
+                .align(Alignment.CenterHorizontally)
+                .clip(CircleShape)
+                .weight(1f)
+                .fillMaxWidth()
+        )
+
         Row(
             modifier = Modifier
                 .fillMaxWidth(0.98f)
@@ -90,7 +126,7 @@ fun AuthScreen(
         ) {
             TextField(
                 value = username,
-                onValueChange = { newValue -> username = newValue },
+                onValueChange = setUsername,
                 label = { Text("Usuário") },
                 leadingIcon = {
                     Icon(
@@ -108,7 +144,6 @@ fun AuthScreen(
 
         Spacer(modifier = Modifier.height(16.dp))
 
-        // Campo de Senha com ícone e botão de visibilidade
         Row(
             modifier = Modifier
                 .fillMaxWidth(0.98f)
@@ -116,8 +151,9 @@ fun AuthScreen(
         ) {
             TextField(
                 value = password,
-                onValueChange = { password = it },
+                onValueChange = setPassword,
                 label = { Text("Senha") },
+
                 leadingIcon = {
                     Icon(
                         imageVector = Icons.Default.Lock,
@@ -146,22 +182,103 @@ fun AuthScreen(
             )
         }
 
-        Spacer(modifier = Modifier.height(24.dp))
+        Spacer(modifier = Modifier.height(2.dp))
 
-        Button(
-            onClick = {
-                onEnterClick(
-                    User(
-                        "1",
-                        username,
-                        password,
-                        "",
-                        EMAIL
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Row(
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                IconButton(onClick = { checkRememberButton = !checkRememberButton }) {
+                    Icon(
+                        imageVector = if (checkRememberButton) Icons.Filled.CheckCircle else Icons.Filled.CheckCircleOutline,
+                        contentDescription = "Lembrar-me",
+                        tint = if (checkRememberButton) MaterialTheme.colorScheme.primary else Color.LightGray
                     )
-                )
-            }) {
+                }
+                Spacer(modifier = Modifier.width(2.dp))
+                Text("Lembrar-me")
+            }
+            TextButton(onClick = {}) {
+                Text("Esqueceu a senha?")
+            }
+        }
+        Spacer(Modifier.height(8.dp))
+        Button(
+            onClick = onLoginClick,
+            modifier = Modifier.fillMaxWidth(0.98f)
+        ) {
             Text(text = "Entrar")
         }
+
+        Spacer(Modifier.height(222.dp))
+
+        AlternativeLoginOptions(
+            onIconClick = { index ->
+                when (index) {
+                    0 -> Toast.makeText(context, "Google Login Click", Toast.LENGTH_SHORT).show()
+                    1 -> Toast.makeText(context, "Instagram Login Click", Toast.LENGTH_SHORT).show()
+                    2 -> Toast.makeText(context, "Github Login Click", Toast.LENGTH_SHORT).show()
+                }
+            },
+            onSignUpClick = onSignUpClick,
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentSize(align = Alignment.BottomCenter)
+        )
+    }
+}
+
+@Composable
+fun AlternativeLoginOptions(
+    onIconClick: (index: Int) -> Unit,
+    onSignUpClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    val iconSize = 42.dp
+    val iconSpacing = 32.dp
+
+    val iconList = listOf(
+        R.drawable.icon_google,
+        R.drawable.icon_instagram,
+        R.drawable.icon_github,
+    )
+
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Ou entre com")
+        Row(horizontalArrangement = Arrangement.SpaceEvenly) {
+            iconList.forEachIndexed { index, iconResId ->
+                if (index > 0) Spacer(Modifier.width(iconSpacing)) // Espaçamento entre ícones
+
+                Icon(
+                    painter = painterResource(iconResId),
+                    contentDescription = "Login Alternativo",
+                    modifier = Modifier
+                        .size(iconSize)
+                        .clickable { onIconClick(index) }
+                        .clip(CircleShape)
+                )
+            }
+        }
+        Spacer(Modifier.width(16.dp))
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text("Sem Cadastro?")
+            Spacer(Modifier.height(6.dp))
+            TextButton(onClick = onSignUpClick) {
+                Text("Cadastrar")
+            }
+        }
+
     }
 }
 
@@ -173,7 +290,7 @@ fun AuthScreenPreview() {
             modifier = Modifier.fillMaxSize(),
             color = MaterialTheme.colorScheme.background
         ) {
-            AuthScreen(onEnterClick = {})
+            AuthScreen({}, {})
         }
     }
 }
--- jetpack-compose-login/app/src/main/java/com/example/loginappwithjetpackcompose/ui/screens/SignInScreen.kt
+++ jetpack-compose-login/app/src/main/java/com/example/loginappwithjetpackcompose/ui/screens/SignInScreen.kt
@@ -1,16 +1,39 @@
 package com.example.loginappwithjetpackcompose.ui.screens
 
 import android.util.Log
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Block
 import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.RemoveRedEye
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
@@ -19,7 +42,7 @@ import com.example.loginappwithjetpackcompose.TipoAutenticacao
 import com.example.loginappwithjetpackcompose.User
 import com.example.loginappwithjetpackcompose.ui.theme.LoginAppWithJetpackComposeTheme
 
-@OptIn(ExperimentalMaterial3Api::class)
+
 @Composable
 fun SignInScreen(
     onEnterClick: (User) -> Unit
@@ -38,6 +61,10 @@ fun SignInScreen(
         var username by remember { mutableStateOf("") }
         var password by remember { mutableStateOf("") }
         var passwordVisible by remember { mutableStateOf(false) }
+        val context = LocalContext.current
+        val (checked, onCheckedChange) = remember {
+            mutableStateOf(false)
+        }
 
         Row(
             modifier = Modifier
@@ -103,6 +130,23 @@ fun SignInScreen(
 
         Spacer(modifier = Modifier.height(24.dp))
 
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Row(
+                modifier = Modifier.weight(1f),  // Add weight to this Row
+                horizontalArrangement = Arrangement.Start, // Align to the start
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Checkbox(checked = checked, onCheckedChange = onCheckedChange)
+                Text("Remember me")
+            }
+            TextButton(onClick = {}) {
+                Text("Forgot Password?")
+            }
+        }
+        Spacer(Modifier.height(8.dp))
         Button(
             onClick = {
                 // Cria o usuário
@@ -118,12 +162,12 @@ fun SignInScreen(
                 // Chama a função onEnterClick com o usuário
                 onEnterClick(user)
             }) {
-            Text(text = "Entrar")
+            Text(text = "Login")
         }
     }
 }
 
-@Preview
+@Preview(showSystemUi = true)
 @Composable
 fun AuthScreenPreview() {
     LoginAppWithJetpackComposeTheme {
